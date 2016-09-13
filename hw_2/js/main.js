705f9d88db93e9beb885d7e08c2ebb4a947841c8
jaenycat22 <jae.k.cheong@gmail.com>
2016-09-13 15:03:34 -0400

    hw2

---
+++ hw_2/js/main.js
@@ -0,0 +1,22 @@
+
+// Start animation to left +300px
+$( "#go" ).click(function() {
+  $( ".block" ).animate({ left: "+=900px" }, 5000 );
+});
+ 
+// Stop animation when button is clicked
+$( "#stop" ).click(function() {
+  $( ".block" ).stop();
+});
+ 
+// Start animation to right +300px
+$( "#back" ).click(function() {
+  $( ".block" ).animate({ left: "-=900px" }, 2000 );
+});
+
+// hide the image when button is clicked 
+$( "cat" ).click(function() {
+  $( "p" ).hide( "slow" );
+});
+
+ 
